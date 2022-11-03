fec17746178a18ebc82b959590e7746a54650e59
LDhimba <Ldhimba@bancabc.co.zw>
2022-11-03 12:59:47 +0200

    custom departmentNotFoundexceptionHandler

---
+++ src/main/java/com/boot/springboottutorial/Entity/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.boot.springboottutorial.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+
+    private HttpStatus status;
+    private String message;
+}
